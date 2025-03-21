1c574e2870418568982a224902a18f8d8a3c5771
Andrija Urosevic <andrija.urosevic27@gmail.com>
2025-03-21 22:34:30 +0100

    Using ThreadPoolExecutor to speed up the scraping. Removed unused function. Scraped all available data.

--- gather-data.py
+++ gather-data.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import date
 
 import pandas as pd
@@ -51,41 +52,42 @@ def save_table_data(gp_table: BeautifulSoup, year: int, section: RaceSection, gp
     print(f'Finished with {section.name}.')
 
 
-def should_scrape(year: int, file_name: str) -> bool:
+def scrape_grand_prix(year: int, url: str, grand_prix: str, gp_id: str, file_name: str):
     for section in RaceSection:
-        if not os.path.exists(f'data/{year}/{section.value}/{file_name}.csv'):
-            return True
-    return False
+        if os.path.exists(f'data/{year}/{section.value}/{file_name}.csv'):
+            continue
+
+        grand_prix_link = f'{url}/{gp_id}/{section.value}'
+
+        try:
+            gp_table = get_table(grand_prix_link, TABLE_CLASS)
+        except ValueError:
+            continue  # In case the race weekend doesn't have the section(for example no sprint qualifying)
+
+        save_table_data(gp_table, year, section, file_name)
 
 
-def scrape_f1_table(year=date.today().year):
+def scrape_f1_table(year=date.today().year, max_workers=10):
     url = f'{F1_BASE_URL}/{year}/races'
 
     try:
         table = get_table(url, TABLE_CLASS)
+        tasks = []
 
-        for i, row in enumerate(table.find('tbody').find_all('tr')):
-            cols = row.find_all('td')
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for i, row in enumerate(table.find('tbody').find_all('tr')):
+                cols = row.find_all('td')
+                grand_prix = cols[0].find('a').text.strip()
+                race_date = cols[1].text.strip()
 
-            grand_prix = cols[0].find('a').text.strip()
-            race_date = cols[1].text.strip()
+                print(f'\n🏁 Gathering data for {grand_prix} {race_date}...')
 
-            print(f'\n🏁 Gathering data for {grand_prix} {race_date}...')
+                file_name = f'{i + 1}-{grand_prix}'
+                gp_id = cols[0].find('a')['href'].split('/', 1)[1].rsplit('/', 1)[0]
+                tasks.append(executor.submit(scrape_grand_prix, year, url, grand_prix, gp_id, file_name))
 
-            file_name = f'{i + 1}-{grand_prix}'
-            gp_id = cols[0].find('a')['href'].split('/', 1)[1].rsplit('/', 1)[0]
-
-            for section in RaceSection:
-                if os.path.exists(f'data/{year}/{section.value}/{file_name}.csv'):
-                    continue
-
-                grand_prix_link = f'{url}/{gp_id}/{section.value}'
-                try:
-                    gp_table = get_table(grand_prix_link, TABLE_CLASS)
-                except ValueError:
-                    continue  # In case the race weekend doesn't have the section(for example no sprint qualifying)
-
-                save_table_data(gp_table, year, section, file_name)
+            for task in as_completed(tasks):
+                task.result()
     except Exception as e:
         print(f'An error occurred: {e}')
 
